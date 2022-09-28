91c86c93f3546c43993846d193fc6f6dd00d02fe
vinoth kumar <91111832+vinothkumar33@users.noreply.github.com>
2022-09-28 20:16:59 +0530

    completed

---
+++ task4-card/src/App.js
@@ -0,0 +1,28 @@
+
+import "./styles.css";
+
+
+function App(){
+     
+  return(
+  <>
+   <div class="a-box">
+     <div class="img-container">
+       <div class="img-inner">
+         <div class="inner-skew">
+           <img  className="in-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pierre-Person.jpg/1200px-Pierre-Person.jpg" />
+         </div>
+       </div>
+     </div>
+     <div class="text-container">
+        <h3>JHON</h3>
+        <h4>Developer</h4>
+        <a href="#">Learn more...</a>
+     </div>
+
+    
+    </div>
+  </>
+  )
+}
+export default App;
